761580d68d0dfad01615fafe7f42c691d96c4a18
umarfarisi <pharese@gmail.com>
2017-10-11 17:58:46 +0700

    init commit

---
+++ src/main/java/com/apap/umarfarisi/tugas/satu/ApapUmarFarisiTugasSatuApplication.java
@@ -0,0 +1,14 @@
+package com.apap.umarfarisi.tugas.satu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapUmarFarisiTugasSatuApplication
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (ApapUmarFarisiTugasSatuApplication.class, args);
+    }
+}
