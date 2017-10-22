fa6900af8d60308fd558ae2e2d3cf846f28406cf
umarfarisi <pharese@gmail.com>
2017-10-22 23:28:19 +0700

    fix errorrrrr

--- src/main/java/com/apap/umarfarisi/tugas/satu/mapper/PendudukMapper.java
+++ src/main/java/com/apap/umarfarisi/tugas/satu/mapper/PendudukMapper.java
@@ -59,7 +59,7 @@ public interface PendudukMapper {
 	@Select("select p.nik, p.nama, p.tempat_lahir as tempatLahir, p.tanggal_lahir as tanggalLahir, p.jenis_kelamin as jenisKelamin, "
 			+ "p.is_wni as wni, p.id_keluarga as idKeluarga, p.agama, p.pekerjaan, p.status_perkawinan as statusPerkawinan, "
 			+ "p.status_dalam_keluarga as statusDalamKeluarga, p.golongan_darah as golonganDarah, p.is_wafat as wafat "
-			+ "from penduduk p, keluarga k, kelurahan kel where p.id_keluarga = k.id &&  k.id_kelurahan = kel.id && kel.id = #{id_kelurahan}")
+			+ "from penduduk p, keluarga k, kelurahan kel where p.id_keluarga = k.id and  k.id_kelurahan = kel.id and kel.id = #{id_kelurahan}")
 	public List<PendudukDBModel> getAllPendudukByIdKelurahan(@Param("id_kelurahan") Long idKelurahan);
 	
 
